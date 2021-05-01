edc27cad9da66f6d15adee8776149303b99164e2
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-01 13:49:21 -0400

    init react-app, cleanup install semantic ui, sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/wrapper.scss";
+import { Button, Container, Card } from "semantic-ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <Card id="quote-box">
+        <Container text class="quote-text">
+          <span id="text"></span>
+        </Container>
+        <div class="quote-author">
+          - <span id="author"></span>
+        </div>
+        <div class="buttons">
+          <a
+            class="button"
+            id="tweet-quote"
+            title="Tweet this quote!"
+            target="_top"
+          >
+            Twitter
+          </a>
+          <a
+            class="button"
+            id="tumblr-quote"
+            title="Post this quote on tumblr!"
+            target="_blank"
+          >
+            Tumblr
+          </a>
+          <Button secondary id="new-quote">
+            New quote
+          </Button>
+        </div>
+      </Card>
+      <div class="footer">
+        {/* by <a href="https://codepen.io/hezag/">hezag</a> */}
+      </div>
     </div>
   );
 }
