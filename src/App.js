b30f2f28d8d63946b2f24125c46be30f47c4539c
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-02 09:56:06 -0400

    add background textures

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       .get(
         "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
       )
-      .then((res) => setQuote(res.data.quotes[Math.ceil(Math.random() * 102)]))
+      .then((res) => setQuote(res.data.quotes[Math.ceil(Math.random() * res.data.quotes.length)]))
       .catch((err) => console.log(err));
   };
 
